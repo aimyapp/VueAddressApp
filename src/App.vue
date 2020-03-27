2b80fb72730b8bd37d0777b8603ab1d67d55e1ac
aimy <aimy.app@gmail.com>
2020-03-27 21:40:56 +0900

    ログイン、ログアウト機能実装

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-side-icon @click="toggleSideMenu"></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click.stop="toggleSideMenu"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span>マイアドレス帳</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-toolbar-items v-if="$store.state.login_user">
+        <v-btn @click="logout">ログアウト</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
     <SideNav/>
 
     <v-content>
-        <router-view/>
+      <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import firebase from 'firebase'
 import SideNav from './components/SideNav'
-
+import { mapActions } from 'vuex'
 export default {
   name: 'App',
   components: {
     SideNav
+  },
+  created () {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setLoginUser(user)
+      } else {
+        this.deleteLoginUser()
+      }
+    })
   },
   data () {
     return {
@@ -30,7 +42,7 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['toggleSideMenu'])
+    ...mapActions(['toggleSideMenu', 'setLoginUser', 'logout', 'deleteLoginUser'])
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,30 @@
+
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router/router.js'
 import store from './store/store.js';
+import firebase from 'firebase';
 
 Vue.config.productionTip = false
 
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAlYRCY7bPdc6Z_vv2W1ttNDSIQj6mIofA",
+  authDomain: "my-address-pj-b0dbc.firebaseapp.com",
+  databaseURL: "https://my-address-pj-b0dbc.firebaseio.com",
+  projectId: "my-address-pj-b0dbc",
+  storageBucket: "my-address-pj-b0dbc.appspot.com",
+  messagingSenderId: "333562252841",
+  appId: "1:333562252841:web:52a42d5b310e265d519638",
+  measurementId: "G-G07WJWTDZK"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+router,
+store,
+render: h => h(App)
 }).$mount('#app')
